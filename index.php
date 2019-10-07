19c309c798a37f86e02e8b45766fa8212edeeaf1
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-10-07 00:14:19 -0400

    feature: added email feature

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hello World
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("kebby@appmail.top","My subject",$msg);
+echo "Mail sent successfully"
+?>
