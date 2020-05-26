1e2838d833979d40be4ff8304f66c46d227252ea
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2020-05-26 12:46:36 -0400

    feature: updated all files

---
+++ index-maintenance.php
@@ -0,0 +1,4 @@
+<?php
+echo "<center><b>This website is under development</b></center>";
+echo "<center>&copy;".date("Y")." <a href='https://fb.com/kaustavbhatt94'>Kaustav Bhattacharjee.</a><center>";
+?>
---
+++ js/config-maintenance.js
@@ -0,0 +1,111 @@
+/* Google MAP Settings*/
+var lat= 22.4873001;  //Change the value with your address Latitude
+var lng= 88.3344841;  //Change the value with your address Longitude
+  
+//Skill Chart Data
+var skillData = {
+	skill1: [
+		{
+			title: "SIEM Administration",
+            color: "#00C5CD",
+			border: "4px solid #f4709f",
+			value: '90%'
+		},
+		{
+			title: "Device Integration with SIEM",
+            color: "#00C5CD",
+			border: "4px solid #f4709f",
+			value: '100%'
+		},
+		{
+			title: "SIEM Implementation & Design",
+            color: "#00C5CD",
+			border: "4px solid #f4709f",
+			value: '105%'
+		},
+        {
+            title: "SecOps using RSA Archer",
+            color: "#00C5CD",
+            border: "4px solid #f4709f",
+            value: '95%'
+        },
+        {
+            title: "Development of Advisory Reports",
+            color: "#00C5CD",
+            border: "4px solid #f4709f",
+            value: '105%'
+        },
+        {
+            title: "Malware Detection & Primary Analysis",
+            color: "#00C5CD",
+            border: "4px solid #f4709f",
+            value: '95%'
+        },
+    
+    
+	],
+	skill2: [
+		{
+			title: "Web Scraping using Python",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '90%'
+		},
+		{
+			title: "Automation using Selenium",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '105%'
+		},
+		{
+			title: "Object Oriented Programming",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '92%'
+		},
+        {
+            title: "DBMS using SQL",
+            color: "#1b7ee4",
+            border: "4px solid #8cc2f9",
+            value: '80%'
+        },
+    {
+        title: "Hadoop & Map Reduce",
+        color: "#1b7ee4",
+        border: "4px solid #8cc2f9",
+        value: '80%'
+		}
+	],
+  skill3: [
+		{
+			title: "PHP",
+            color: "#00CC66",
+			border: "4px solid #e4f9b1",
+			value: '95%'
+		},
+      {
+          title: "Javascript",
+          color: "#00CC66",
+          border: "4px solid #e4f9b1",
+          value: '85%'
+      },
+		{
+			title: "HTML & CSS",
+            color: "#00CC66",
+			border: "4px solid #e4f9b1",
+			value: '80%'
+		},
+    {
+			title: "AJAX & JQuery",
+            color: "#00CC66",
+			border: "4px solid #e4f9b1",
+			value: '75%'
+		},
+		{
+			title: "Linux",
+            color: "#00CC66",
+			border: "4px solid #e4f9b1",
+			value: '90%'
+		}
+	]
+}
